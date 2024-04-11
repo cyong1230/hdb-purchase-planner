051801e325c74dceb4f41deb7725636252a74864
ramcharan-singh <ram_charans@outlook.com>
2024-04-11 12:30:26 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -27,6 +27,8 @@ def run_query(query):
 
 rows = run_query("SELECT COUNT(*) as count_row FROM `skillful-elf-416113.hdb.hdb_resale_final` LIMIT 1000")
 
+pd.read_gbq("SELECT COUNT(*) as count_row FROM `skillful-elf-416113.hdb.hdb_resale_final` LIMIT 1000", credentials=credentials)
+
 # Print results.
 st.write("Total Count: ")
 for row in rows:
